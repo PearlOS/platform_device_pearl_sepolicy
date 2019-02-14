28d1f42e2f058fe9e6abfb41885ed705dd8d3f7f
dianlujitao <dianlujitao@lineageos.org>
2019-02-14 20:11:01 +0800

    sepolicy: Move livedisplay hal policy to dynamic
    Change-Id: I7d7b932688d95fc397576e74bc1ff434395ec663

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -3,9 +3,17 @@
 # that inherit from Lineage
 #
 
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/pearl/sepolicy/qcom/private
 
+ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
     device/pearl/sepolicy/qcom/common \
     device/pearl/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+endif
